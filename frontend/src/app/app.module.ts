d1610a28eeab74899e7176bbc416fb99f2b395fa
Mike Boan <mikeboan@gmail.com>
2018-04-21 18:08:08 -0400

    add first route

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,10 +8,11 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
